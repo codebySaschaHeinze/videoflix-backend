e4878bb23438485df6c6be330d5c9a1b8af95103
Sascha Heinze <sascha.heinze.dev@gmail.com>
2026-04-27 14:13:25 +0200

    feat(content): add video model and admin registration

--- content/models.py
+++ content/models.py
@@ -1,1 +1,27 @@
 from django.db import models
+
+
+class Video(models.Model):
+    """Store video metadata"""
+
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    category = models.CharField(max_length=50)
+
+    thumbnail = models.ImageField(
+        upload_to='thumbnails/',
+    )
+
+    source_file = models.FileField(
+        upload_to='videos/',
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
