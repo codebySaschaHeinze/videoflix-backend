0f621f7604f616c1f116a848ce0f0a1dcd15d1dd
Sascha Heinze <sascha.heinze.dev@gmail.com>
2026-05-06 13:37:56 +0200

    chore(content): improve video admin overview

--- content/admin.py
+++ content/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
+
 from content.models import Video
 
 
-admin.site.register(Video)
+@admin.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+    """Configure video display in Django admin."""
+
+    list_display = (
+        'title',
+        'category',
+        'processing_status',
+        'created_at',
+        'updated_at',
+    )
+    list_filter = ('category', 'processing_status')
+    search_fields = ('title', 'description')
+    readonly_fields = (
+        'created_at',
+        'updated_at',
+        'processing_error',
+    )
