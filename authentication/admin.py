8d52a2270b728870e01ee96c8e5a34f9a7daffd0
Sascha Heinze <sascha.heinze.dev@gmail.com>
2026-05-08 23:47:29 +0200

    feat(authentication): register custom user model in admin

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,64 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from authentication.models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    """Configure custom user model in Django admin."""
+
+    model = User
+
+    list_display = (
+        'email',
+        'is_active',
+        'is_staff',
+        'is_superuser',
+        'date_joined',
+    )
+
+    list_filter = (
+        'is_active',
+        'is_staff',
+        'is_superuser',
+        'groups',
+    )
+
+    search_fields = ('email',)
+
+    ordering = ('email',)
+
+    fieldsets = (
+        (None, {
+            'fields': ('email', 'password'),
+        }),
+        ('Permissions', {
+            'fields': (
+                'is_active',
+                'is_staff',
+                'is_superuser',
+                'groups',
+                'user_permissions',
+            ),
+        }),
+        ('Important dates', {
+            'fields': ('last_login', 'date_joined'),
+        }),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            ),
+        }),
+    )
+
+    readonly_fields = ('last_login', 'date_joined')
